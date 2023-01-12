5cbac32f99d5d96916275a0230bd9d17b7173fa3
ChengShusss <cheng.s.shu@foxmail>
2023-01-12 22:50:50 +0800

    finished ch1

---
+++ ch1/1.7-9_fetchWithoutCache/fetch.go
@@ -0,0 +1,35 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// 1.8
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		// 1.7
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		// 1.9
+		fmt.Printf("Status Code: %v\n", resp.Status)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
